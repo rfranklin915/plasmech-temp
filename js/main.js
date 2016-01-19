6b408eb02982f84c256607f62a818b51605c3d5e
Richard Franklin <richard.franklin@designagency.co.uk>
2016-01-19 16:58:06 +0000

    building homepage front-end

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var skrol = skrollr.init();
+
+var wow = new WOW(
+  {
+      boxClass: 'anim-in',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+  }
+);
+wow.init();
