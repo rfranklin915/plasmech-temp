530a58a70cafad59ac4138427cdb53f4a462e003
Richard Franklin <richard.franklin@designagency.co.uk>
2016-01-21 12:00:43 +0000

    inner pages built - news - sectors - inc. tabs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var skrol = skrollr.init();
+var skrol = skrollr.init({
+    forceHeight: false
+});
 
 var wow = new WOW(
   {
@@ -8,10 +10,44 @@ var wow = new WOW(
 );
 wow.init();
 
+//tabs function
+var pageTabs = function () {
+    if ($('.tab-area').length > 0) {
+        $('.tab-area .tab .open-tab a').click(function () {
 
-$(document).ready(function(){
-    $('.article-img-slider').slick({
-        arrows: false,
-        dots: true
-    });
+            var parentTab = $(this).parents('.tab');
+            var slideSpeed = 400;
+
+            if (parentTab.hasClass('active')) {
+                //if tab active
+                parentTab.removeClass('active');
+                parentTab.find('.tab-inner').slideUp(slideSpeed);
+            }
+            else {
+                //if tab not active
+                $('.tab-area .tab').removeClass('active');
+                $('.tab-area .tab .tab-inner').slideUp(slideSpeed);
+                parentTab.addClass('active');
+                parentTab.find('.tab-inner').slideDown(slideSpeed);
+            };
+
+            return false;
+        });
+    }
+};
+
+var slickSliderInit = function () {
+    if ($('.article-img-slider').length > 0) {
+        $('.article-img-slider').slick({
+            arrows: false,
+            dots: true
+        });
+    }
+};
+
+
+$(document).ready(function () {
+    slickSliderInit();
+    pageTabs();
+
 });
